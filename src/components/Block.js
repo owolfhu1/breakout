87a8ba5fc13478921e9d99fc8b50b8e07bc905e3
Orion Wolf_Hubbard <owolfhu1@gmail.com>
2018-09-07 06:54:55 -0400

    working

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,45 +1,89 @@
 import React from 'react';
 
-
-
 class Block extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            //color : props.color
-            alive : true,
-            health : 1 || this.props.health,
-            invincible: false || this.props.invincible,   
-            //ok so i made it so that Block has a function it can call it suicide, props.die()
-            //it also gets props.ball.x and props.ball.y so we need something in the render method that
-            //will will this.props.die() if the ball touchs it    
+            color: 'blue',
+            // alive : true,
+            // health: 1 || this.props.health,
+            // invincible: false || this.props.invincible,
         }
     }
 
 
+    //i need to make it bounce
+
+    render() {
+
+        let { ball, left, bottom, width, height } = this.props;
+        let ballCenterX = ball.x + ball.radius;
+        let ballCenterY = ball.y - ball.radius;// its at left
+        let blockCenterX = left + width / 2;
+        let blockCenterY = bottom - height / 2;//fucking niec
+        let xDist = Math.abs(ballCenterX - blockCenterX);
+        let yDist = Math.abs(ballCenterY - blockCenterY);
+        let cornerDist = (xDist - width / 2) ^ 2 +
+            (yDist - height / 2) ^ 2;
+        if (
+            !(xDist > (width / 2 + ball.radius)) &&
+            !(yDist > (height / 2 + ball.radius))
+        ) {
+            if (
+                (xDist <= (width / 2)) ||
+                (yDist <= (height / 2)) ||
+                (cornerDist <= (ball.radius ^ 2))
+            ) {
+                if (ballCenterY < blockCenterY) {
+                    this.props.die('top', true);
+                } else if (ballCenterY > blockCenterY) {
+                    this.props.die('bottom', true);
+                } else if (ballCenterX < blockCenterX) {
+                    this.props.die('left', true);
+                } else if (ballCenterX > blockCenterX) {
+                    this.props.die('right', true);
+                } else {
+                    this.props.die('bottom', true);
+                }
+            }
+        }
+
+        /*
+
+                ball = {
+                    x: this.state.ballX,
+                    y: this.state.ballY,
+                    radius: this.state.radius,
+                    direction : {
+                        x: this.state.XDir,
+                        y: this.state.YDir
+                    }
+                }
 
 
-    render () {
-        let {ball, left, bottom} = this.props;
+                    ^
+                    |+ball.direction.y
+                    |
+                  ____ v--(ball.x + 2*ball.radius, ball.y + 2*ball.radius)
+                |/    \|
+                |      | -->+ball.direction.x
+                |\____/|
+                ^--(ball.x, ball.y)
 
-        if (ball.x > bottom && 
-            ball.x < bottom + 10 && 
-            ball.y > left && 
-            ball.y < left + 30)
-            this.props.die();
+        */
 
 
         return (
-                <div style={{
-                    position : 'absolute',
-                    bottom : `${this.props.bottom}px` , 
-                    left : `${this.props.left}px`,
-                    height : '15px',
-                    width : '34px',
-                    background : this.state.color,
-                    border : '2px solid black', 
-                }}/>
+            <div style={{
+                position: 'absolute',
+                bottom: `${this.props.bottom}px`,
+                left: `${this.props.left}px`,
+                height: `${this.props.height}px`,
+                width: `${this.props.width}px`,
+                background: this.state.color,
+                border: '2px solid black',
+            }} />
         )
     }
 
@@ -48,6 +92,3 @@ class Block extends React.Component {
 
 export default Block;
 
-//eric can you go into game.js constructor and re-write 
-//the blocks array to hold objects like i started? i made the first one
-//ok
--- src/components/Game.js
+++ src/components/Game.js
@@ -19,39 +19,55 @@ class Game extends React.Component {
             speed : 5,
             movingLeft:false,
             movingRight:false,
-            paddleWidth: 100,
+            paddleWidth: 20,
             paddle : 200,
             paddleY : 0,
             canJump:true,
-            jumping:false,     
+            jumping:false,
             XDir:1,
             YDir:1,
-            ballX : 100,
-            ballY : 20,
+            ballX : 200,
+            ballY : 200,
+            radius : 5,
             interval : null,
 
             blocks: [
 
-                {bottom:20,left:40,health:2},
-                {bottom:40,left:40,health:2},
-                {bottom:60,left:40,health:2},
-                {bottom:80,left:40,health:2},
-                {bottom:20,left:80,health:2},
-                {bottom:40,left:80,health:2},
-                {bottom:60,left:80,health:2},
-                {bottom:80,left:80,health:2},
-            
-            
+                {bottom:300,left:40,health:2},
+                {bottom:320,left:40,health:2},
+                {bottom:340,left:40,health:2},
+                {bottom:360,left:40,health:2},
+                {bottom:300,left:120,health:2},
+                {bottom:320,left:120,health:2},
+                {bottom:340,left:120,health:2},
+                {bottom:360,left:120,health:2},
+                {bottom:300,left:200,health:2},
+                {bottom:320,left:200,health:2},
+                {bottom:340,left:200,health:2},
+                {bottom:360,left:200,health:2},
+                {bottom:300,left:270,health:2},
+                {bottom:320,left:270,health:2},
+                {bottom:340,left:270,health:2},
+                {bottom:360,left:270,health:2},
+                {bottom:200,left:200,health:2},
+                {bottom:220,left:200,health:2},
+                {bottom:240,left:200,health:2},
+                {bottom:260,left:200,health:2},
+                {bottom:200,left:270,health:2},
+                {bottom:220,left:270,health:2},
+                {bottom:240,left:270,health:2},
+                {bottom:260,left:270,health:2},
+
 
                 //dont need index thats automatic lol
-                //im going to work on a funciton, you can make this 
+                //im going to work on a funciton, you can make this
                 //programaticly instead of typing it all if you want, or jsut amke a few
 
-                
+
             ],
         };
-        this.moveBall = this.moveBall.bind(this);
-    }
+        this.moveBall = this.moveBall.bind(this);//ok it sort of works, try that
+    }//go to line 178 come on
 
     componentDidMount() {
         this.windowDiv.focus();
@@ -61,11 +77,11 @@ class Game extends React.Component {
     moveBall() {
 
         if(this.state.movingRight){
-            if(this.state.paddle<=(500-this.state.paddleWidth)-this.state.speed){                
+            if(this.state.paddle<=(500-this.state.paddleWidth)-this.state.speed){
                 this.setState({paddle:this.state.paddle + this.state.speed});
             }else{
                 this.setState({paddle:(500-this.state.paddleWidth)});
-            } 
+            }
         }else if(this.state.movingLeft){
             if(this.state.paddle>=this.state.speed){
                 this.setState({paddle:this.state.paddle - this.state.speed});
@@ -80,28 +96,45 @@ class Game extends React.Component {
         }else if(this.state.jumping){
             this.setState({paddleY:this.state.paddleY + 5});
         }else if(this.state.paddleY>0){
-            if(this.state.paddleY>5){ 
+            if(this.state.paddleY>5){
                 this.setState({paddleY:this.state.paddleY - 5});
             }else{
-                this.setState({paddleY:0});                
+                this.setState({paddleY:0});
             }
-        }        
-        
-        
-        if ((this.state.XDir > 0 && this.state.ballX > 480) ||
+        }
+
+
+        if ((this.state.XDir > 0 && this.state.ballX > 500 - 2*this.state.radius) ||
          (this.state.XDir < 0 && this.state.ballX < 1)) {
             this.setState({
                 XDir : this.state.XDir*-1
-            })            
+            })
         }
 
-        if ((this.state.YDir > 0 && this.state.ballY > 480) ||
-         (this.state.YDir < 0 && this.state.ballY < this.state.paddleY && 
-            this.state.paddle -10 < this.state.ballX && 
+        if ((this.state.YDir > 0 && this.state.ballY > 500 - 2*this.state.radius) ||
+         (this.state.YDir < 0 && this.state.ballY < this.state.paddleY &&
+            this.state.paddle -10 < this.state.ballX &&
             this.state.paddle +10 > this.state.ballX - this.state.paddleWidth)) {
-            this.setState({YDir : this.state.YDir * -1 + 
-                ((this.state.jumping && this.state.ballY < this.state.paddleY) ? 1 : 0 )})  
-                    
+
+            let zone = this.state.ballX - this.state.paddle + this.state.radius;
+
+            //debugger
+            //alert(zone)
+
+            let YDir = this.state.YDir < 0 && !this.state.jumping ? 1 :
+                (this.state.YDir * -1 + ((this.state.jumping && this.state.ballY < this.state.paddleY) ? 1 : 0 ))
+
+
+            //let YDir = this.state.YDir * -1 + ((this.state.jumping && this.state.ballY < this.state.paddleY) ? 1 : 0 );
+
+            //let XDir = this.state.YDir > 0 ? this.state.XDir : (zone < 30 ? -2 : zone > this.state.width - 30 ? 2 : this.state.XDir)
+            let XDir = this.state.YDir > 0 ? this.state.XDir :
+                zone < this.state.paddleWidth/5 ? -2 :
+                zone > this.state.paddleWidth - this.state.paddleWidth/5 ? 2 :
+                this.state.XDir > 0 ? 1 : -1;
+
+            this.setState({YDir,XDir})
+
         }
 
         this.setState({
@@ -122,7 +155,7 @@ class Game extends React.Component {
             width : this.state.paddleWidth+'px',
             background : 'black',
             bottom : this.state.paddleY+'px',
-            left : `${this.state.paddle}px`,            
+            left : `${this.state.paddle}px`,
         }
     }
 
@@ -133,11 +166,11 @@ class Game extends React.Component {
                 break;
             }
             case 'ArrowRight' : {
-                this.setState({movingRight:false});            
+                this.setState({movingRight:false});
                 break;
             }
         }
-    }//what wait?
+    }
 
     handleKeyDown(e) {
 
@@ -147,15 +180,15 @@ class Game extends React.Component {
                 break;
             }
             case 'ArrowRight' : {
-                this.setState({movingRight:true});             
+                this.setState({movingRight:true});
                 break;
             }
             case 'ArrowUp' : {
                 if(this.state.canJump&&this.state.paddleY===0){
                     this.setState({jumping:true});
-                }                           
+                }
                 break;
-            }            
+            }
         }
 
     }
@@ -166,18 +199,32 @@ class Game extends React.Component {
             position : 'absolute',
             bottom : `${this.state.ballY}px`,
             left : `${this.state.ballX}px`,
-            width : '20px',
-            height : '20px',
+            width : 2*this.state.radius + 'px',
+            height : 2*this.state.radius + 'px',
             background : 'red',
-            borderRadius : '10px'
+            borderRadius : this.state.radius + 'px',
         }
     }
 
     killYourSelf(index) {
-        return () => {
-            let blocks = this.state.blocks;
-            delete blocks[index];
-            this.setState({blocks});
+        return (side,die) => {
+            switch(side) {
+                case 'left' :
+                case 'right' :
+                    this.setState({XDir:this.state.XDir*-1});
+                    break;
+                case 'top' :
+                case 'bottom' :
+                    this.setState({YDir:this.state.YDir*-1});
+                    break;
+                default :
+                    console.log('Error in killYourSelf()');
+            }
+            if (die) {
+                let blocks = this.state.blocks;
+                delete blocks[index];
+                this.setState({blocks});
+            }
         }
     }
 
@@ -185,13 +232,20 @@ class Game extends React.Component {
         let list = [];
         for (let i = 0; i < this.state.blocks.length; i++) {
             if (this.state.blocks[i])
-            list.push(<Block bottom={this.state.blocks[i].bottom} 
-                             left={this.state.blocks[i].left} 
-                             die={this.killYourSelf(i)} 
+            list.push(<Block bottom={this.state.blocks[i].bottom}
+                             left={this.state.blocks[i].left}
+                             die={this.killYourSelf(i)}
                              health={this.state.blocks[i].health}
+                             width={50}
+                             height={15}
                              ball={{
                                  x:this.state.ballX,
-                                 y:this.state.ballY
+                                 y:this.state.ballY,
+                                 radius:this.state.radius,
+                                 direction : {
+                                     x: this.state.XDir,
+                                     y: this.state.YDir
+                                 }
                              }}
                               />);
             }
@@ -201,14 +255,12 @@ class Game extends React.Component {
     render() {
         return (
             <div ref={div => {this.windowDiv = div;}}
-                 tabIndex="0" 
+                 tabIndex="0"
                  style={style}
                  onKeyDown={this.handleKeyDown.bind(this)}
                  onKeyUp={this.handleKeyUp.bind(this)}>
-                
-                    {this.drawBlocks()}
-                
-                
+
+                {this.drawBlocks()}
 
                 <span style={this.getBallStyle()}></span>
                 <span style={this.getPaddleStyle()}></span>
